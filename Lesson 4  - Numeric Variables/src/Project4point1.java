c26b0691e0480038d99463e7933dcf524cce95d0
Zaid Abu Jumaiza <zabujumaiza@HS-D210-07.MOHS.local>
2019-03-06 14:19:24 -0500

    yerr

---
+++ Lesson 4  - Numeric Variables/src/Project4point1.java
@@ -0,0 +1,24 @@
+
+public class Project4point1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 79;
+		int b = 3;
+		int c = 4;
+		int d = 82;
+		int e = 68;
+		int f = 7;
+		int g = 19;
+		int h = 179;
+		int i = 21;
+		int j = 10;
+		int k = 181;
+		int l = 10389;
+		int m = 56;
+		int n = 11;
+		int o = 2246;
+		
+	}
+
+}
---
+++ Lesson 4  - Numeric Variables/src/exercise4point1.java
@@ -0,0 +1,60 @@
+
+public class exercise4point1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int h = 103;
+		int p = 5;
+		System.out.println(++h+p);
+		System.out.println(h);
+		
+		double def;
+		double f = 1992.37;
+		def = f;
+		System.out.println(def);
+		
+		int zulu = 2;
+		System.out.println(--zulu);
+		
+		int a = 100;
+		int b = 200;
+		b/=a;
+		System.out.println(b+1);
+		
+		int v = 60;
+		v-=30;
+		
+		v=v-30;
+		
+		int x = 40;
+		int q = 4;
+		System.out.println(2 + 8 * q / 2 - x);
+		
+		int sd = 12;
+		int x2 = 4;
+		System.out.println(sd%(++x2));
+		System.out.println(x2);
+		
+		double m1,b1,f1 = 3.14;
+		
+		int x1,y1,z1;
+		
+		int m2 = 36;
+		int j2 = 5;
+		m2 = m2 / j2;
+		System.out.println(m2);
+		
+		int j =2;
+		System.out.println(7%3+j++ +(j-2));
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
