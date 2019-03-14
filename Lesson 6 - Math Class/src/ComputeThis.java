aef212e265b2b1971ff3840b28cd9d52a4f6b702
Zaid Abu Jumaiza <zabujumaiza@HS-D210-07.MOHS.local>
2019-03-14 12:11:05 -0400

    lesson 7 start

--- Lesson 6 - Math Class/src/ComputeThis.java
+++ Lesson 6 - Math Class/src/ComputeThis.java
@@ -3,7 +3,16 @@ public class ComputeThis {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		double d1 = 3*Math.PI*Math.sin(Math.toRadians(187))+Math.abs(Math.cos(Math.toRadians(122)));
+		double d2 = Math.pow(14.72,3.801)+Math.log(72);
+		
+		System.out.println("d1 = "+d1);
+		System.out.println("d2 = "+d2);
+		
+		
+		
+		
+		
 	}
 
 }
