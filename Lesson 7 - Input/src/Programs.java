f8278b8b91e86f6a2373bc62da48975a9c652be0
Zaid Abu Jumaiza <zabujumaiza@HS-D210-07.MOHS.local>
2019-03-18 14:19:41 -0400

    uooo

---
+++ Lesson 7 - Input/src/Programs.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Programs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Scanner x = new Scanner(System.in);
+			System.out.println("Please enter your whole name.");
+			int count = x.nextLine().length();
+			System.out.println("Your name has "+count+"characters in it, how many would you like me to print?");
+			int count2 = x.nextInt()
+			
+			
+	}
+
+}
